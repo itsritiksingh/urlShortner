54a4444a280010e904d7d3099f08cc4a0e2b2a18
ritik singh <mr.ritik99@gmail.com>
2020-02-07 23:59:48 +0530

    initial release

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+var fs = require('fs');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+app.use(fileUpload());
+app.use(express.urlencoded({extended:false}))
+app.use(express.static("./public"));
+app.use(express.json());
+
+const BitlyClient = require("bitly").BitlyClient;
+const bitly = new BitlyClient("a57af3cd3c6478a0880ea3390ec1d1245e3b1555");
+
+app.post("/generateLink", (req, res) => {
+  const url = req.body.url;
+
+  bitly
+    .shorten(url)
+    .then(function(result) {
+        console.log(result)
+       res.json(result);
+    })
+    .catch(function(error) {
+      return res.json(error);
+    });
+});
+
+app.get('*',(req,res)=>{
+    var html = fs.createReadStream('public/index.html');
+    html.pipe(res);
+})
+
+app.listen(5000,()=> console.log('server started'));
